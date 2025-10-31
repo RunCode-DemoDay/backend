91c01cd36919b781a5783879f299d7ab00781c06
dia3065 <dja306500@gmail.com>
2025-11-01 03:56:49 +0900

    Feat:러너 유형 수정 및 로그아웃 구현

--- src/main/java/com/RunCode/user/controller/UserController.java
+++ src/main/java/com/RunCode/user/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.RunCode.user.controller;
 
+import com.RunCode.common.domain.ApiResponse;
+import com.RunCode.user.dto.UserRegisterResponse;
 import com.RunCode.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RequiredArgsConstructor
 @RestController
@@ -14,15 +18,18 @@ public class UserController {
 
     // 현재 로그인된 사용자 정보 조회
     @GetMapping("/me")
-    public ResponseEntity<?> getUserInfo(@RequestHeader("Authorization") String authHeader) {
+    public ResponseEntity<ApiResponse<UserRegisterResponse>> getUserInfo(
+            @RequestHeader("Authorization") String authHeader) {
         return userService.getUserInfo(authHeader);
     }
+
     // 현재 로그인된 사용자의 러너 유형 변경
     @PatchMapping("/me")
-    public ResponseEntity<?> updateRunnerType(
+    public ResponseEntity<ApiResponse<UserRegisterResponse>> updateRunnerType(
             @RequestHeader("Authorization") String authHeader,
-            @RequestParam("typeId") Long typeId
-    ) {
+            @RequestBody Map<String, Object> body) {
+        Long typeId = Long.valueOf(body.get("type_id").toString());
         return userService.updateRunnerType(authHeader, typeId);
     }
+
 }
--- src/main/java/com/RunCode/user/repository/UserRepository.java
+++ src/main/java/com/RunCode/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.RunCode.user.repository;
 
 import com.RunCode.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +11,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByKakaoId(String kakaoId);
-
+    @Query("select u from User u left join fetch u.type where u.id = :id")
+    Optional<User> findByIdWithType(@Param("id") Long id);
 }
